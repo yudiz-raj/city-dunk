02db5f17fce338273970118d684467e72a92141c
yudiz-raj <raj.patel@yudiz.com>
2023-09-11 16:58:13 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,62 @@
+const init = () => {
+	class Boot extends Phaser.Scene {
+		preload() {
+			this.load.pack("pack", "assets/preload-asset-pack.json");
+			this.load.on(Phaser.Loader.Events.COMPLETE, () =>
+				this.scene.start("Preload")
+			);
+		}
+	}
+	if (window.innerWidth < 1050) {
+		var game = new Phaser.Game({
+			width: 720,
+			height: 1180,
+			type: Phaser.AUTO,
+			backgroundColor: "#000000",
+			parent: "game-division",
+			scale: {
+				mode: Phaser.Scale.FIT,
+				autoCenter: Phaser.Scale.CENTER_BOTH,
+				orientation: Phaser.Scale.Orientation.PORTRAIT,
+			},
+			audio: {
+				disableWebAudio: false,
+			},
+			dom: {
+				createContainer: true,
+			},
+		});
+	} else {
+		var game = new Phaser.Game({
+			width: 1920,
+			height: 1080,
+			type: Phaser.AUTO,
+			backgroundColor: "#00000",
+			parent: "game-division",
+			scale: {
+				mode: Phaser.Scale.FIT,
+				autoCenter: Phaser.Scale.CENTER_BOTH,
+			},
+			audio: {
+				disableWebAudio: false,
+			},
+			dom: {
+				createContainer: true,
+			},
+			physics: {
+				default: "arcade",
+                arcade: {
+                    gravity: { y: 0 },
+					debug: true,
+                },
+			}
+		});
+	}
+	game.scene.add("Preload", Preload);
+	game.scene.add("Level", Level);
+	game.scene.add("Boot", Boot, true);
+};
+
+window.onload = (event) => {
+	init();
+};
---
+++ src/scenes/Level.js
@@ -0,0 +1,207 @@
+
+// You can write more code here
+let nScore = 0;
+/* START OF COMPILED CODE */
+
+class Level extends Phaser.Scene {
+
+	constructor() {
+		super("Level");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		// body
+		const body = this.add.container(0, 0);
+
+		// world_rect
+		const world_rect = this.add.rectangle(960, 540, 1920, 1080);
+		body.add(world_rect);
+
+		// container_upperRings
+		const container_upperRings = this.add.container(0, 0);
+		body.add(container_upperRings);
+
+		// container_ball
+		const container_ball = this.add.container(0, 0);
+		body.add(container_ball);
+
+		// container_collider
+		const container_collider = this.add.container(0, -1);
+		body.add(container_collider);
+
+		// container_lowerRings
+		const container_lowerRings = this.add.container(0, 0);
+		body.add(container_lowerRings);
+
+		// scoreTxt
+		const scoreTxt = this.add.text(960, 70, "", {});
+		scoreTxt.setOrigin(0.5, 0.5);
+		scoreTxt.setStyle({ "fontSize": "60px" });
+		body.add(scoreTxt);
+
+		this.world_rect = world_rect;
+		this.container_upperRings = container_upperRings;
+		this.container_ball = container_ball;
+		this.container_collider = container_collider;
+		this.container_lowerRings = container_lowerRings;
+		this.scoreTxt = scoreTxt;
+
+		this.events.emit("scene-awake");
+	}
+
+	/** @type {Phaser.GameObjects.Rectangle} */
+	world_rect;
+	/** @type {Phaser.GameObjects.Container} */
+	container_upperRings;
+	/** @type {Phaser.GameObjects.Container} */
+	container_ball;
+	/** @type {Phaser.GameObjects.Container} */
+	container_collider;
+	/** @type {Phaser.GameObjects.Container} */
+	container_lowerRings;
+	/** @type {Phaser.GameObjects.Text} */
+	scoreTxt;
+
+	/* START-USER-CODE */
+
+	// Write more your code here
+	generateRing() {
+
+		setInterval(() => {
+			if (this.container_upperRings.list[this.container_upperRings.list.length - 1].x < 2000) {
+
+				const nRandomX = Math.floor(Math.random() * (720 - 590)) + 590;
+				const nRandomY = Math.floor(Math.random() * (778 - 272)) + 272;
+				const uperRing = this.ringGroup.create(this.container_upperRings.list[this.container_upperRings.list.length - 1].x + nRandomX, nRandomY, "upperRing");
+				uperRing.body.immovable = true;
+				uperRing.body.setSize(20, 1);
+				uperRing.body.setOffset(342, 255);
+				this.container_upperRings.add(uperRing);
+
+				const lowerRing = this.ringGroup.create(uperRing.x, uperRing.y + 30, "lowerRing");
+				lowerRing.body.immovable = true;
+				lowerRing.body.setSize(20, 1);
+				lowerRing.body.setOffset(130, 225);
+				this.container_lowerRings.add(lowerRing);
+
+				const graphics = this.add.graphics();
+				graphics.fillRect(0, 0, 128, 0.2);
+				const rectTexture = graphics.generateTexture();
+				const rectangle = this.colliderGroup.create(uperRing.x, uperRing.y + 20, rectTexture);
+				rectangle.body.immovable = true;
+				rectangle.body.setSize(128, 0.2);
+				rectangle.body.setOffset(-50, 15);
+				rectangle.setVisible(false);
+				this.container_collider.add(rectangle);
+			}
+		}, 1000);
+	}
+
+	handleBall(ball) {
+		this.input.keyboard.createCursorKeys();
+		this.input.keyboard.on("keydown", (event) => {
+			if (event.code == "Space") {
+				ball.body.setVelocityY(-1200);
+			}
+		}, this);
+
+		this.input.on("pointerdown", () => {
+			ball.body.setVelocityY(-1200);
+			ball.angle -= 1;
+		});
+	}
+
+	create() {
+		this.editorCreate();
+
+		this.ballsGroup = this.physics.add.group();
+		this.ringGroup = this.physics.add.group();
+		this.colliderGroup = this.physics.add.group();
+
+		const ball = this.ballsGroup.create(430, 460, "ball");
+		ball.setCircle(ball.width / 2.7, 25, 25);
+		ball.setScale(0.5, 0.5);
+		ball.setName("ball");
+		ball.setCollideWorldBounds();
+		ball.weight = 1000;
+		this.container_ball.add(ball);
+		ball.body.setGravityY(2000);
+
+		const nRandomX = Math.floor(Math.random() * (700 - 500)) + 500;
+		const nRandomY = Math.floor(Math.random() * (778 - 272)) + 272;
+		const uperRing = this.ringGroup.create(ball.x + nRandomX, nRandomY, "upperRing");
+		uperRing.body.immovable = true;
+		uperRing.body.setSize(20, 1);
+		uperRing.body.setOffset(342, 255);
+		this.container_upperRings.add(uperRing);
+
+		const lowerRing = this.ringGroup.create(uperRing.x, uperRing.y + 30, "lowerRing");
+		lowerRing.body.immovable = true;
+		lowerRing.body.setSize(20, 1);
+		lowerRing.body.setOffset(130, 225);
+		this.container_lowerRings.add(lowerRing);
+
+		const graphics = this.add.graphics();
+		graphics.fillRect(0, 0, 128, 0.02);
+		const rectTexture = graphics.generateTexture();
+		const rectangle = this.colliderGroup.create(uperRing.x, uperRing.y + 20, rectTexture);
+		rectangle.body.immovable = true;
+		rectangle.body.setSize(128, 0.02);
+		rectangle.body.setOffset(-50, 15);
+		rectangle.setVisible(false);
+		this.container_collider.add(rectangle);
+
+		this.generateRing();
+		this.handleBall(ball);
+
+		this.scoreTxt.setText("Score: " + nScore);
+		this.physics.add.collider(ball, this.ringGroup);
+		this.ringCollider = this.physics.add.collider(ball, this.colliderGroup, (ball, collider) => {
+			if (ball.y <= collider.y) {
+				collider.body.checkCollision.up = false;
+			}
+			else {
+				if (collider.body.checkCollision.up) {
+					this.scene.pause("Level");
+				}
+			}
+		});
+	}
+
+	update() {
+		this.container_collider.list.forEach((collider) => {
+			collider.x -= 6;
+			if (collider.x < this.container_ball.list[0].x - 200 && collider.body.checkCollision.up) {
+				this.scene.pause("Level");
+			}
+			if (collider.x < -128) {
+				nScore += 5;
+				this.scoreTxt.setText("Score: " + nScore);
+				collider.destroy();
+			}
+		});
+		this.container_lowerRings.list.forEach((ring) => {
+			ring.x -= 6;
+			if (ring.x < -128) {
+				ring.destroy();
+			}
+		});
+		this.container_upperRings.list.forEach((ring) => {
+			ring.x -= 6;
+			if (ring.x < -128) {
+				ring.destroy();
+			}
+		})
+	}
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
